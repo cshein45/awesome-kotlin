71ddd387d27ffb59134f5580047a1fcf520d45d2
Dmitry Nekrasov <scannorone@gmail.com>
2026-02-25 11:56:47 +0400

    Add kodvent - algorithms and data structures library for Advent of Code (#1070)

--- src/main/resources/links/Libraries.awesome.kts
+++ src/main/resources/links/Libraries.awesome.kts
@@ -1347,6 +1347,11 @@ category("Libraries/Frameworks") {
   }
   subcategory("Misc") {
     link {
+      github = "DmitryNekrasov/kodvent"
+      desc = "Kotlin Multiplatform utility library with battle-tested, zero-dependency implementations of algorithms and data structures for competitive programming and Advent of Code."
+      setTags("algorithms", "data-structures", "competitive-programming", "advent-of-code", "multiplatform")
+    }
+    link {
       github = "RationalityFrontline/kevent"
       desc = "A powerful in-process event dispatcher based on Kotlin and Coroutines."
       setTags("eventbus", "event-dispatcher", "event-driven", "publish–subscribe", "android", "swing", "javafx", "coroutines", "functional-programming")
